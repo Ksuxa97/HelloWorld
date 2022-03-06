08b7d046b2fe1830e1a1ead8388a4379448ae33a
Kseniya Ignatyeva <kignatyeva@ourgapps.com>
2022-03-06 17:29:10 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kseniya Ignatyeva on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
